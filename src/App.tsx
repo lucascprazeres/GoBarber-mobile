25c4c3c68bd668324eb6c006aa93f97dc55df5d5
lucascprazeres <olucascostadp@gmail.com>
2020-07-05 12:22:47 -0300

    minor: adiciona logo e fonte customizada à tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Routes from './routes';
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+    <View style={{ flex: 1, backgroundColor: '#312e38d' }}>
       <Routes />
     </View>
   </NavigationContainer>
