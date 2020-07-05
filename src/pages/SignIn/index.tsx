ec156d46efde1eb56720f18fb491d0409a767eee
lucascprazeres <olucascostadp@gmail.com>
2020-07-05 15:57:18 -0300

    feat: adiciona componentes de Input e Button personalizados, à tela de logon

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoimg from '../../assets/logo.png';
+
 import { Container, Title } from './styles';
-import logo from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Image source={logo} />
+      <Image source={logoimg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('placeholder')}>Entrar</Button>
     </Container>
   );
 };
